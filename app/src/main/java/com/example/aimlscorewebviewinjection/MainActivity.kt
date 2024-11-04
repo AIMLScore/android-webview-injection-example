a3a0a238b555365a7cf192c11f5f3d7bb53c80ba
Daniel Ryan <d@nielryan.com>
2024-11-04 16:25:13 +0100

    feat: adding cookie persistence

--- app/src/main/java/com/example/aimlscorewebviewinjection/MainActivity.kt
+++ app/src/main/java/com/example/aimlscorewebviewinjection/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aimlscorewebviewinjection
 
 import android.os.Bundle
+import android.webkit.CookieManager
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -9,13 +10,23 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  // Set your layout here
+        setContentView(R.layout.activity_main)
 
         val webView: WebView = findViewById(R.id.webview)
-        webView.webViewClient = WebViewClient()  // Ensures links open in the WebView instead of a browser
+
+        // Ensures links open in the WebView instead of a browser
+        webView.webViewClient = WebViewClient()
 
         val webSettings: WebSettings = webView.settings
-        webSettings.javaScriptEnabled = true  // Enable JavaScript if needed
+        webSettings.javaScriptEnabled = true
+
+        // Enable persistent cookies
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
+        cookieManager.setAcceptThirdPartyCookies(webView, true)
+
+        // This enables persistent storage of cookies across sessions
+        CookieManager.getInstance().flush()
 
         // Exchange the URL here for your client Frontend URL.
         webView.loadUrl("https://www.example.com/")
